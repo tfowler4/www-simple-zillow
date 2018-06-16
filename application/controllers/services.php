0b267980b2493d0ea4bc11f9b276db17f66ca914
Terrijon Fowler <tfowler4@student.gsu.edu>
2018-06-16 00:02:24 -0400

    Testing file_get_contents

--- application/controllers/services.php
+++ application/controllers/services.php
@@ -20,14 +20,40 @@ class Services extends AbstractController {
     }
 
     public function getSearchResults() {
-        $curl = curl_init();
-
-        // http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz1ghhd0anevf_5upah&address=2325+Valley+Creek+Drive&citystatezip=30122
-    
-        // https://www.zillow.com/search/GetResults.htm?spt=homes&status=110001&lt=111101&ht=001000&pr=200000,500000&mp=803,2008&bd=5%2C&ba=3.0%2C&sf=3000,&lot=0%2C&yr=,&singlestory=0&hoa=0%2C&pho=0&pets=0&parking=0&laundry=0&income-restricted=0&fr-bldg=0&condo-bldg=0&furnished-apartments=0&cheap-apartments=0&studio-apartments=0&pnd=0&red=0&zso=0&days=any&ds=all&pmf=1&pf=1&sch=100111&zoom=10&rect=-84709397,33930542,-83971253,34159261&p=1&sort=days&search=map&rid=54219&rt=6&listright=true&isMapSearch=1&zoom=10
-
         // Get cURL resource
         $curl = curl_init();
+/*
+        $parameters = array(
+            'p' =>,
+            'ds' =>,
+            'zso' =>,
+            'pnd' =>,
+            'pho' =>,
+            'red' =>,
+            'rt' =>,
+            'ht' =>,
+            'lt' =>,
+            'yr' =>,
+            'lot' =>,
+            'sf' =>,
+            'ba' =>,
+            'pr' =>,
+            'bd' =>,
+            'pets' =>,
+            'parking' =>,
+            'laundry' =>,
+            'pmf'     =>,
+            'pf'      =>,
+            'zoom'    =>,
+            'sort'    =>,
+            'att'     =>,
+            'days'    =>,
+            'rid'      =>,
+            'spt'      =>,
+            '**kwargs' =>,
+            'rect'    =>
+        );
+*/
 
         // Setting the HTTP Request Headers
         $User_Agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36';
@@ -45,26 +71,13 @@ class Services extends AbstractController {
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, TRUE);
         curl_setopt($curl, CURLOPT_COOKIESESSION, TRUE);
         curl_setopt($curl, CURLOPT_AUTOREFERER, TRUE);
-        curl_setopt($curl, CURLOPT_HEADER, TRUE);
         curl_setopt($curl, CURLOPT_COOKIEJAR, 'cookie.txt');
         curl_setopt($curl, CURLOPT_COOKIEFILE, 'cookie.txt');
 
-        // Send the request & save response to $resp
-        $response = curl_exec($curl);
-        //$parser   = xml_parser_create();
-        //$results  = array();
-
-        // Close request to clear up some resources
-        //curl_close($curl);
-
-        //xml_parse_into_struct($parser, $response, $results);
-
-        //xml_parser_free($parser);
-
-        //$response = file_get_contents(urlencode('https://www.zillow.com/search/GetResults.htm?spt=homes&status=110001&lt=111101&ht=001000&pr=200000,500000&mp=803,2008&bd=5%2C&ba=3.0%2C&sf=3000,&lot=0%2C&yr=,&singlestory=0&hoa=0%2C&pho=0&pets=0&parking=0&laundry=0&income-restricted=0&fr-bldg=0&condo-bldg=0&furnished-apartments=0&cheap-apartments=0&studio-apartments=0&pnd=0&red=0&zso=0&days=any&ds=all&pmf=1&pf=1&sch=100111&zoom=10&rect=-84709397,33930542,-83971253,34159261&p=1&sort=days&search=map&rid=54219&rt=6&listright=true&isMapSearch=1&zoom=10'));
-echo "<BR>START RESULTS<BR>";
+        //$response = curl_exec($curl);
+        $response = file_get_contents('https://www.zillow.com/search/GetResults.htm?spt=homes&status=110001&lt=111101&ht=111111&pr=,900000&mp=,3602&bd=0%2C&ba=0%2C&sf=,&lot=0%2C&yr=,&singlestory=0&hoa=0%2C&pho=0&pets=0&parking=0&laundry=0&income-restricted=0&fr-bldg=0&condo-bldg=0&furnished-apartments=0&cheap-apartments=0&studio-apartments=0&pnd=0&red=0&zso=0&days=any&ds=all&pmf=1&pf=1&sch=100111&zoom=11&rect=-84715577,33898204,-84346505,34029188&p=1&sort=days&search=map&rid=12562&rt=6&listright=true&isMapSearch=true&zoom=11');
         print_r($response);
-        echo "<BR>END RESULTS<BR>";
+
         //print_r(json_encode($results));
     }
 }
